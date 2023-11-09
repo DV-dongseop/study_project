c764d84ef37a774072fe35caa337087a73881617
동균 한 <hdg5639@naver.com>
2023-11-09 14:32:31 +0900

    original

--- app/src/main/java/com/example/final_studymatching_project/Test.kt
+++ app/src/main/java/com/example/final_studymatching_project/Test.kt
@@ -1,16 +1,4 @@
 package com.example.final_studymatching_project
 
 class Test {
-    //ddddddddd
-    /**
-     * 시
-     * 발
-     *때
-     * 려
-     * 쳐
-     *
-     *
-     *
-     */
-    /
 }
