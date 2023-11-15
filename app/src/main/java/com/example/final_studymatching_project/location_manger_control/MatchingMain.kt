a738005e6326d9a669fd573be3949cb50ac7c18f
hdg5639@stu.kmu.ac.kr <hdg5639@stu.kmu.ac.kr>
2023-11-15 19:00:37 +0900

    Button

--- app/src/main/java/com/example/final_studymatching_project/location_manger_control/MatchingMain.kt
+++ app/src/main/java/com/example/final_studymatching_project/location_manger_control/MatchingMain.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.final_studymatching_project.R
 
-class Mylocation : AppCompatActivity() {
+class MatchingMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mylocation)
+        setContentView(R.layout.activity_matching_main)
     }
 }
