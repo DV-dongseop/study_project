83b2e1c0b0ac97b0a0ab827290e3f9ebf4d9302d
DV-dongseop <ehdtjq1278@nvaer.com>
2023-11-16 12:53:43 +0900

    btn1

--- app/src/main/java/com/example/final_studymatching_project/MainActivity.kt
+++ app/src/main/java/com/example/final_studymatching_project/MainActivity.kt
@@ -1,46 +1,54 @@
 package com.example.final_studymatching_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.final_studymatching_project.location_manger_control.MatchingMain
 import com.example.final_studymatching_project.ui.theme.Final_StudyMatching_ProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Final_StudyMatching_ProjectTheme {
-                // A surface container using the 'background' color from the theme
+            Final_StudyMatching_ProjectTheme() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun MainContent() {
+        // XML에서 정의된 버튼 사용
+        Button(
+            onClick = { navigateToMatchingMain() },
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(text = "Matching")
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Final_StudyMatching_ProjectTheme {
-        Greeting("Android")
+    private fun navigateToMatchingMain() {
+        val intent = Intent(this, MatchingMain::class.java)
+        startActivity(intent)
+
+
     }
 }
