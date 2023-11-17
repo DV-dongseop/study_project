8e8b09391b132c40d7a4dd07237e97d0c040b159
hdg5639@stu.kmu.ac.kr <hdg5639@stu.kmu.ac.kr>
2023-11-17 17:05:44 +0900

    Sibal backup 2222222

--- app/src/main/java/com/example/final_studymatching_project/MainActivity.kt
+++ app/src/main/java/com/example/final_studymatching_project/MainActivity.kt
@@ -2,63 +2,24 @@ package com.example.final_studymatching_project
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Space
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.final_studymatching_project.location_manger_control.MatchingMain
 import com.example.final_studymatching_project.ui.theme.Final_StudyMatching_ProjectTheme
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-import com.google.firebase.database.database
-import com.google.firebase.ktx.Firebase
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Final_StudyMatching_ProjectTheme() {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainContent()
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn1)
+        button.setOnClickListener {
+            val intent = Intent(this, MatchingMain::class.java)
+            startActivity(intent)
+            finish()
         }
-        //파이어베이스 실시간 데이터 체킹용
-        val database = com.google.firebase.Firebase.database
-        val myRef = database.getReference("Checking")
-        myRef.setValue("Success")
-    }
-
-    @Composable
-    fun MainContent() {
-        // XML에서 정의된 버튼 사용
-        Button(
-            onClick = { navigateToMatchingMain() },
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(text = "Matching")
-        }
-    }
-
-    private fun navigateToMatchingMain() {
-        val intent = Intent(this, MatchingMain::class.java)
-        startActivity(intent)
-
-
     }
 }
-
--- app/src/main/java/com/example/final_studymatching_project/location_manger_control/Matchingmain.kt
+++ app/src/main/java/com/example/final_studymatching_project/location_manger_control/Matchingmain.kt
@@ -1,30 +1,55 @@
 package com.example.final_studymatching_project.location_manger_control
 
-
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.final_studymatching_project.MainActivity
 import com.example.final_studymatching_project.R
-
+import com.example.final_studymatching_project.location_manger_control.MyLocation
+import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MatchingMain : AppCompatActivity() {
+    private val TAG = "MatchingMain"
+    private lateinit var myLocation: MyLocation
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matchingmain)
 
-        // study_matching_btn 클릭 시 Studylist 액티비티로 이동
-        val studyMatchingBtn = findViewById<Button>(R.id.study_matching_btn)
-        studyMatchingBtn.setOnClickListener {
-            val intent = Intent(this, Studylist::class.java)
-            startActivity(intent)
-        }
+        // MyLocation 인스턴스 생성
+        myLocation = MyLocation(this)
 
-        findViewById<Button>(R.id.study_createbtn).setOnClickListener {
-            val intent = Intent(this@MatchingMain, Roomcrate::class.java)
-            startActivity(intent)
-        }
+        // 위치 권한 부여 및 확인
+        myLocation.requestLocationPermission()
 
+        // 위치 정보 저장을 위한 함수 호출
+        myLocation.startLocationUpdates()
+
+
+        val homeButton: Button = findViewById(R.id.home_btn)
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val createButton: Button = findViewById(R.id.study_createbtn)
+        createButton.setOnClickListener{
+            val intent = Intent(this, Roomcrate::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
-}
 
+}
